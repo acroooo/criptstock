99537edbd8c9a60f74959b7e0bbd971a2c37d942
Hernán Chamorro <hernang.work@gmail.com>
2022-08-11 14:41:14 -0300

    'antd-and-navbar'

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import { Switch, Route, Link } from 'react-router-dom'
-import {Layout, Typography, Space} from 'antd';
+import { Layout, Typography, Space } from 'antd';
+import { Navbar } from './components/index'
+import './App.css'
 
 const App = () => {
     return (
         <div>
             <div className="App">
                 <div className="navbar">
-
+                    <Navbar />
                 </div>
                 <div className="main">
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,31 @@
 import React from 'react'
 import { Button, Menu, Typography, Avatar } from 'antd';
-import { Link } from 'react-router-dom'
+import {  Link } from 'react-router-dom'
 import { HomeOutlined, MoneyCollectOutlined, BulbOutlined, FundOutlined, MenuOutlined } from '@ant-design/icons';
-import icon from './../assets/images/icon.png'
+import icon from '../assets/images/cryptostock.png'
 
 const Navbar = () => {
   return (
         <div className="nav-container">
           <div className="logo-container">
-              <Avatar src={icon} size="large" />
+              <Avatar src={icon} size={25} />
               <Typography.Title level={2} className="logo">
                   <Link to="/">CryptoStock</Link>
               </Typography.Title>
-              {/* <Button className="menu-control-container">
-              </Button> */}
+              <Menu theme="dark">
+                <Menu.Item icon={<HomeOutlined />}>
+                  <Link to="/">Home</Link>
+                </Menu.Item>
+                <Menu.Item icon={<FundOutlined />}>
+                  <Link to="/cryptocurrencies">Cryptocurrencies</Link>
+                </Menu.Item>
+                <Menu.Item icon={<MoneyCollectOutlined />}>
+                  <Link to="/exchanges">Exchanges</Link>
+                </Menu.Item>
+                <Menu.Item icon={<HomeOutlined />}>
+                  <Link to="/news">News</Link>
+                </Menu.Item>
+              </Menu>
           </div>
         </div>
   )
