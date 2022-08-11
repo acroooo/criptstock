1530fcbf63fbf7b81e0faf982a78396d1cdcfaad
Hernán Chamorro <hernang.work@gmail.com>
2022-08-11 12:41:32 -0300

    'structure_index_app'

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Switch, Route, Link } from 'react-router-dom'
+import {Layout, Typography, Space} from 'antd';
+
+const App = () => {
+    return (
+        <div>
+            <div className="App">
+                <div className="navbar">
+
+                </div>
+                <div className="main">
+
+                </div>
+                <div className="footer">
+                    
+                </div>
+        </div>
+    );
+}
+
+export default App
